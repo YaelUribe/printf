d9746f13d5c8573a97cffb6f2f1279cfa6503d8e
Hugo Cortes <hugocortesmu@gmail.com>
2021-03-14 17:49:50 -0500

    revisarnumber

--- _printf.c
+++ _printf.c
@@ -3,21 +3,20 @@
 *
 *
 */
-int _printf(const char *format, ...);
-if (format == NULL) 
-{ 
-    return 0;
+int _printf(const char *format, ...)
+{
+if (format == NULL)
+{
+	return (0);
 }
 va_list va_printf;
-va_start (va_printf, format);
+va_start(va_printf, format);
 
-int number = 0; // revisar //
+int number = 0; /* revisar */
 int green = 0;
 int leng = _strlen(format);
 int y = 0;
-
-
-       if ((format[y] == '%') && ((y + 1) <leng))
-
+if ((format[y] == '%') && ((y + 1) < leng))
+}
 
 
