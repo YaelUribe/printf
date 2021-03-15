0e6d70fa845ebeaad0a6db80a19d13e0ac9b3e7d
Hugo Cortes <hugocortesmu@gmail.com>
2021-03-15 16:37:15 -0500

    change prototype

--- holberton.h
+++ holberton.h
@@ -3,8 +3,9 @@
 #include <stdarg.h>
 #include <unistd.h>
 int _printf(const char *format, ...);
-int _strlen(char *s);
-char _printf_s(char *s);
-char _printf_c(char *s);
+int _strlen(va_list va_printf);
+char _printf_s(va_list va_printf);
+char _printf_c(va_list va_printf);
 int _printf_d(int a);
+int (*get_operator(const char c))(va_list va_printf);
 #endif /* HOLBERTON_H */
