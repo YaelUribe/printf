f7253b539060db0fd489a44919cecdc84e843f3e
Yaeluribe <2962@holbertonschool.com>
2021-03-15 19:24:41 -0500

    library updated, struct

--- holberton.h
+++ holberton.h
@@ -4,11 +4,17 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+struct operator 
+{
+	char tipo;
+	int (*fn)(va_list);
+}oper;
 int _printf(const char *format, ...);
-int _strlen(va_list va_printf);
+typedef struct operator oper;
+int _strlen(char *s);
 char _printf_s(va_list va_printf);
 char _printf_c(va_list va_printf);
-int _printf_d(int a);
+int _printf_d(va_list va_printf);
 int (*get_operator(const char c))(va_list va_printf);
 int aval(char c);
 #endif /* HOLBERTON_H */
