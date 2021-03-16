849bdbe696e5c683142ab037019bda7b89467930
Yaeluribe <2962@holbertonschool.com>
2021-03-16 16:34:53 -0500

    changes in functions and some definitions :rocket:

--- _printf.c
+++ _printf.c
@@ -13,37 +13,38 @@ int _printf(const char *format, ...)
 	va_list va_printf;
 
 	int buffer;
-	int leng = _strlen(format);
 	int y;
 
 	for (y = 0; format[y] != '\0'; y++)
 	{
 		buffer = 0;
 		va_start(va_printf, format);
-		if ((format[y] == '%') && ((y + 1) < leng))
+		if (format[y] == '%')
+		{
+			y++;
+			if (aval(format[y]))
 			{
+				buffer += get_operator(format[y])(va_printf);
 				y++;
-				if (aval(format[y]))
-				{
-					buffer += get_operator(format[y])(va_printf);
-				}
-				else if (format[y] == '\0')
-				{
-					return (-1);
-				}
-				else if (format[y] == '%')
-				{
-					buffer += _write('%');
-				}
-				else if(format[y] != '\0')
-				{
-					buffer += _write(format[y]);
-				}
 			}
-			buffer += _write(format[y])
+			else if (format[y] == '\0')
+			{
+				return (-1);
+			}
+			else if (format[y] == '%')
+			{
+				buffer += _write('%');
+			}
+			else if (format[y] != '\0')
+			{
+				buffer += _write(format[y]);
+			}
+		}
+		buffer += _write(format[y]);
 	}
 	va_end(va_printf);
-	return (buffer); // (write(1, buffer, 1024);
+
+	return (buffer);
 }
 /**
  *aval - see if given char matches with operators
@@ -59,7 +60,7 @@ int aval(char c)
  *@f: char given
  *Return: -1 if error, char if success
  */
-char _write(f)
+int _write(char f)
 {
 	return (write(1, &f, 1));
 }
