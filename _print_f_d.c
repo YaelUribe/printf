feedcfa4ea10432a2e93e01dd4b234e080e4801d
Yaeluribe <2962@holbertonschool.com>
2021-03-16 15:02:42 -0500

    functions for an integer or decimal

--- _print_f_d.c
+++ _print_f_d.c
@@ -1,35 +1,74 @@
+#include "holberton.h"
 /**
  *_printf_d - Prints a decimal (base 10) number
- *@a: number to print
+ *@va_printf: number to print
  *Return: our number ready to print
  */
 int _printf_d(va_list va_printf)
 {
-	int number = 0;
-	int dummie = number;
-	int cadena[1024];
-	int e = 0;
+	int number, signo = 1, digitos = 0, d, base;
+	unsigned int dummie;
+	char numberz[1024];
 
+	number = va_arg(va-printf, int), digitos, signo;
+	dummie = number <  0  ? (unsigned int) number * -1 : (unsigned int) number;
 	if (number < 0)
 	{
-		number = -number;
+		signo = -1;
 	}
-	while (number != 0)
+	else if ( number == 0)
 	{
-		cadena[e++] = (number % 10);
-		number /= 10;
+		return (write(1, "0", 1));
 	}
-	if (dummie < 0)
+	for (signo == -1)
 	{
-		cadena[e++] = '-';
-		e--;
+		d = 0, numberz[d] = '-', d++;
 	}
-	while (e >= 0)
-	{
-		if (cadena[e] != '-')
-		return (cadena[e--] + '0');
-		else
-		return (cadena[e--]);
-	}
+	digitos =  _digitos(dummie);
+	base = _divider(digitos);
 
+	while (base >0)
+	{
+		numberz[d] = '-';
+		dummie %= base;
+		base /= 10;
+		d++;
+	}
+	if (signo == -1)
+	{
+		digitos++;
+	}
+	return (write(1,numberz, digitos));
+}
+/**
+ * _divider - to get the first digit on any number
+ * @digitos:number of digits to divide
+ * Return: 1*10^digitos-1 for instance 2458 -> 2000
+ */
+int _divider(int digitos)
+{
+	int base = 1;
+
+	exponente = digitos;
+	while ( exponente > 1)
+	{
+		base *= 10;
+		exponente--;
+	}
+	return (base);
+}
+/**
+ *_digitos - determines amount o digits when dummie >= 10
+ *@dummie:unsigned of int to analize
+ *Return: digits of dummie
+ */
+int _digitos(unsigned int dummie)
+{
+	int digit;
+
+	for (digit = 0, dummie > 0; digit++)
+	{
+		dummie /= 10;
+	}
+	return (digit);
 }
