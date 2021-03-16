dc71e9149f1ef3e9f1250da3c116ff141392bec0
Hugo Cortes <hugocortesmu@gmail.com>
2021-03-16 12:06:46 -0500

    add the coments

--- holberton.h
+++ holberton.h
@@ -4,18 +4,23 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-struct operator 
+/**
+ *structure that determines identifier
+ *@tipo: char representing data type to print
+ *@fn: pointer to function to print data
+ */
+struct operator
 {
 	char tipo;
 	int (*fn)(va_list);
-}oper;
+};
 int _printf(const char *format, ...);
 typedef struct operator oper;
-int _strlen(char *s);
-char _printf_s(va_list va_printf);
-char _printf_c(va_list va_printf);
-int _printf_d(va_list va_printf);
-int (*get_operator(const char c))(va_list va_printf);
-int aval(char c);
-char _write(f);
+int _strlen(char *s); /* string length*/
+char _printf_s(va_list va_printf); /*print string*/
+char _printf_c(va_list va_printf); /*print char*/
+int _printf_d(va_list va_printf); /*print digits, integers*/
+int (*get_operator(const char c))(va_list va_printf);/*determines operator to use*/
+int aval(char c);/*validates char with operator*/
+char _write(f); /* putchar-like function*/
 #endif /* HOLBERTON_H */
