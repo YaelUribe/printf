643c9c08a0c70c85297cd5783ca39e40e25c3b43
Yaeluribe <2962@holbertonschool.com>
2021-03-17 13:52:09 -0500

    corrected additions

--- _printf.c
+++ _printf.c
@@ -31,12 +31,10 @@ int _printf(const char *format, ...)
 			}
 			else if (format[y + 1] != '\0')
 			{
-				y++;
 				buffer += _write(format[y]);
 			}
 			else if (format[y + 1] == '%')
 			{
-				y++;
 				_write('%');
 			}
 			y++;
